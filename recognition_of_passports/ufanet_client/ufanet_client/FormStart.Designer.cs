59dcfd45fe0d89b11e22ed15c47c4de90772db71
Umrikhin <bono39@yandex.ru>
2021-11-25 11:54:19 +0300

    веб служба для распозавания паспорта РФ

---
+++ recognition_of_passports/ufanet_client/ufanet_client/FormStart.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace ufanet_client
+{
+    partial class FormStart
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormStart));
+            this.tabControlMain = new System.Windows.Forms.TabControl();
+            this.tabPagePhoto = new System.Windows.Forms.TabPage();
+            this.labelRez = new System.Windows.Forms.Label();
+            this.buttonRec = new System.Windows.Forms.Button();
+            this.buttonClearPhoto = new System.Windows.Forms.Button();
+            this.buttonBrowsePhoto = new System.Windows.Forms.Button();
+            this.pictureBoxPhoto = new System.Windows.Forms.PictureBox();
+            this.openFileDialogPhoto = new System.Windows.Forms.OpenFileDialog();
+            this.tabControlMain.SuspendLayout();
+            this.tabPagePhoto.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPhoto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControlMain
+            // 
+            this.tabControlMain.Controls.Add(this.tabPagePhoto);
+            this.tabControlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControlMain.Location = new System.Drawing.Point(0, 0);
+            this.tabControlMain.Name = "tabControlMain";
+            this.tabControlMain.SelectedIndex = 0;
+            this.tabControlMain.Size = new System.Drawing.Size(500, 475);
+            this.tabControlMain.TabIndex = 1;
+            // 
+            // tabPagePhoto
+            // 
+            this.tabPagePhoto.Controls.Add(this.labelRez);
+            this.tabPagePhoto.Controls.Add(this.buttonRec);
+            this.tabPagePhoto.Controls.Add(this.buttonClearPhoto);
+            this.tabPagePhoto.Controls.Add(this.buttonBrowsePhoto);
+            this.tabPagePhoto.Controls.Add(this.pictureBoxPhoto);
+            this.tabPagePhoto.Location = new System.Drawing.Point(4, 22);
+            this.tabPagePhoto.Name = "tabPagePhoto";
+            this.tabPagePhoto.Size = new System.Drawing.Size(492, 449);
+            this.tabPagePhoto.TabIndex = 0;
+            this.tabPagePhoto.Text = "Фото";
+            // 
+            // labelRez
+            // 
+            this.labelRez.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelRez.Location = new System.Drawing.Point(259, 20);
+            this.labelRez.Name = "labelRez";
+            this.labelRez.Size = new System.Drawing.Size(210, 327);
+            this.labelRez.TabIndex = 4;
+            this.labelRez.Text = "Результат:";
+            // 
+            // buttonRec
+            // 
+            this.buttonRec.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonRec.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonRec.Location = new System.Drawing.Point(368, 367);
+            this.buttonRec.Name = "buttonRec";
+            this.buttonRec.Size = new System.Drawing.Size(80, 23);
+            this.buttonRec.TabIndex = 3;
+            this.buttonRec.Text = "Распознать";
+            this.buttonRec.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // buttonClearPhoto
+            // 
+            this.buttonClearPhoto.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonClearPhoto.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonClearPhoto.Location = new System.Drawing.Point(272, 367);
+            this.buttonClearPhoto.Name = "buttonClearPhoto";
+            this.buttonClearPhoto.Size = new System.Drawing.Size(80, 23);
+            this.buttonClearPhoto.TabIndex = 1;
+            this.buttonClearPhoto.Text = "Очистить";
+            this.buttonClearPhoto.Click += new System.EventHandler(this.buttonClearPhoto_Click);
+            // 
+            // buttonBrowsePhoto
+            // 
+            this.buttonBrowsePhoto.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonBrowsePhoto.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonBrowsePhoto.Location = new System.Drawing.Point(272, 399);
+            this.buttonBrowsePhoto.Name = "buttonBrowsePhoto";
+            this.buttonBrowsePhoto.Size = new System.Drawing.Size(80, 23);
+            this.buttonBrowsePhoto.TabIndex = 2;
+            this.buttonBrowsePhoto.Text = "Прикрепить";
+            this.buttonBrowsePhoto.Click += new System.EventHandler(this.buttonBrowsePhoto_Click);
+            // 
+            // pictureBoxPhoto
+            // 
+            this.pictureBoxPhoto.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxPhoto.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxPhoto.Location = new System.Drawing.Point(18, 20);
+            this.pictureBoxPhoto.Name = "pictureBoxPhoto";
+            this.pictureBoxPhoto.Size = new System.Drawing.Size(236, 406);
+            this.pictureBoxPhoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxPhoto.TabIndex = 0;
+            this.pictureBoxPhoto.TabStop = false;
+            // 
+            // openFileDialogPhoto
+            // 
+            this.openFileDialogPhoto.Filter = "Изображения (*.jpg; *.jpeg; *.bmp; *.png)|*.jpg;*.jpeg;*.bmp;*.png";
+            // 
+            // FormStart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 475);
+            this.Controls.Add(this.tabControlMain);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormStart";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ufanet Client";
+            this.tabControlMain.ResumeLayout(false);
+            this.tabPagePhoto.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPhoto)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControlMain;
+        private System.Windows.Forms.TabPage tabPagePhoto;
+        private System.Windows.Forms.Label labelRez;
+        private System.Windows.Forms.Button buttonRec;
+        private System.Windows.Forms.Button buttonClearPhoto;
+        private System.Windows.Forms.Button buttonBrowsePhoto;
+        private System.Windows.Forms.PictureBox pictureBoxPhoto;
+        private System.Windows.Forms.OpenFileDialog openFileDialogPhoto;
+    }
+}
+
---
+++ recognition_of_passports/ufanet_client/ufanet_client/FormStart.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace ufanet_client
+{
+    public partial class FormStart : Form
+    {
+        string Host = "passrec.ufanet.ru/api/v0";
+
+        //Пути для служб
+        string PathService = "/passports/recognition/";
+        string PathServiceToken = "/token/";
+
+        //Параметры доступа
+        string login = "demo_panaceya";
+        string password = "We0t2GgcZIMR";
+
+        public FormStart()
+        {
+            InitializeComponent();
+            ReadSettings();
+        }
+
+        private void ReadSettings()
+        {
+            if (!(System.IO.File.Exists(Application.StartupPath + "\\ufanet_client.ini")))
+            {
+                return;
+            }
+            IniFile clsIni = new IniFile(Application.StartupPath + "\\ufanet_client.ini");
+            login = clsIni.IniReadValue("Settings", "login");
+            password = clsIni.IniReadValue("Settings", "password");
+        }
+        private void buttonBrowsePhoto_Click(object sender, EventArgs e)
+        {
+            if (openFileDialogPhoto.ShowDialog() == DialogResult.OK)
+            {
+                FileInfo fi = new FileInfo(openFileDialogPhoto.FileName);
+
+                pictureBoxPhoto.Image = Image.FromFile(openFileDialogPhoto.FileName);
+                labelRez.Text = "Результат:";
+            }
+        }
+
+        private void buttonClearPhoto_Click(object sender, EventArgs e)
+        {
+            pictureBoxPhoto.Image = null;
+            labelRez.Text = "Результат:";
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            if (pictureBoxPhoto.Image == null)
+            {
+                labelRez.Text = "Результат:\r\nПрикрепите файл изображения.";
+                return;
+            }
+            HttpStatusCode code = HttpStatusCode.NotFound;
+            var output = SendData(ImageToBase64(pictureBoxPhoto.Image, System.Drawing.Imaging.ImageFormat.Jpeg), out code);
+            if (code == HttpStatusCode.OK)
+            {
+                var result = JsonConvert.DeserializeObject<Models.PassportRecognition>(output).detail;
+                labelRez.Text = "Результат:\r\n" + "Серия и номер: " + result.series_number;
+                labelRez.Text += "\r\n" + "Кем выдан: " + result.authority;
+                labelRez.Text += "\r\n" + "Дата выдачи: " + result.issue_date;
+                labelRez.Text += "\r\n" + "Код подразделения: " + result.authority_code;
+                labelRez.Text += "\r\n" + "Фамилия: " + result.surname;
+                labelRez.Text += "\r\n" + "Имя: " + result.name;
+                labelRez.Text += "\r\n" + "Отчество: " + result.patronymic;
+                labelRez.Text += "\r\n" + "Пол: " + result.gender;
+                labelRez.Text += "\r\n" + "Дата рождения: " + result.birthday;
+                labelRez.Text += "\r\n" + "Место рождения: " + result.birthplace;
+            }
+            else if (code == HttpStatusCode.BadRequest)
+            {
+                var result = JsonConvert.DeserializeObject<Models.BodyResultRec400>(output);
+                labelRez.Text = "Результат:\r\n" + result.detail;
+                labelRez.Text += "\r\n" + result.error_detail;
+                labelRez.Text += "\r\n" + result.status;
+                labelRez.Text += "\r\n" + result.status_id;
+                labelRez.Text += "\r\n" + result.error_message;
+            }
+            else if (code == HttpStatusCode.Forbidden)
+            {
+                var result = JsonConvert.DeserializeObject<Models.BodyResultRec403>(output);
+                labelRez.Text = "Результат:\r\n" + result.detail;
+                labelRez.Text += "\r\n" + result.error_detail;
+                labelRez.Text += "\r\n" + result.status;
+                labelRez.Text += "\r\n" + result.status_id;
+                labelRez.Text += "\r\n" + result.error_message;
+            }
+            else
+            {
+                labelRez.Text = "Результат:\r\nРезультат не получен. " + output;
+            }
+        }
+
+        public string SendData(string image_base64, out HttpStatusCode statusCode)
+        {
+            statusCode = HttpStatusCode.NotFound;
+
+            String ResHTML = "";
+
+            HttpWebRequest Req;
+            HttpWebResponse resp;
+            Stream receiveStream;
+            StreamReader readStream;
+
+            try
+            {
+                System.Net.ServicePointManager.ServerCertificateValidationCallback +=
+delegate(object sender, System.Security.Cryptography.X509Certificates.X509Certificate certificate,
+                        System.Security.Cryptography.X509Certificates.X509Chain chain,
+                        System.Net.Security.SslPolicyErrors sslPolicyErrors)
+{
+    return true; // **** Always accept
+};
+
+                Req = (HttpWebRequest)WebRequest.Create("https://" + Host + PathService);
+                Req.Method = "POST";
+                Req.AllowAutoRedirect = true;
+                Req.ContentType = " application/json";
+
+                bool isGetToken = false;
+                var token = GetAccessToken(out isGetToken);
+                if (!isGetToken) throw new Exception("Токен доступа не получен: " + token);
+                Req.Headers.Add("Authorization", "JWT " + token);
+
+                Models.BodyRequest body = new Models.BodyRequest() { file = image_base64, doc_type = "passport" };
+                byte[] sentData = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(body));
+                Req.ContentLength = sentData.Length;
+                Req.GetRequestStream().Write(sentData, 0, sentData.Length);
+                Req.GetRequestStream().Close();
+                resp = (HttpWebResponse)Req.GetResponse();
+                statusCode = resp.StatusCode;
+                receiveStream = resp.GetResponseStream();
+                readStream = new StreamReader(receiveStream, System.Text.Encoding.UTF8);
+                ResHTML = readStream.ReadToEnd();
+                resp.Close();
+                readStream.Close();
+
+            }
+            catch (Exception exc)
+            { ResHTML = exc.Message; }
+            return ResHTML;
+        }
+
+        string GetAccessToken(out bool isGetToken)
+        {
+            HttpStatusCode statusCodeResult = HttpStatusCode.NotFound;
+            isGetToken = false;
+
+            String ResHTML = "";
+
+            HttpWebRequest Req;
+            HttpWebResponse resp;
+            Stream receiveStream;
+            StreamReader readStream;
+
+            try
+            {
+                Req = (HttpWebRequest)WebRequest.Create("https://" + Host + PathServiceToken);
+                Req.Method = "POST";
+                Req.AllowAutoRedirect = true;
+                Req.ContentType = " application/json";
+                Models.CredentialForToken body = new Models.CredentialForToken() { login = login, password = password };
+                byte[] sentData = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(body));
+                Req.ContentLength = sentData.Length;
+                Req.GetRequestStream().Write(sentData, 0, sentData.Length);
+                Req.GetRequestStream().Close();
+                resp = (HttpWebResponse)Req.GetResponse();
+                statusCodeResult = resp.StatusCode;
+                receiveStream = resp.GetResponseStream();
+                readStream = new StreamReader(receiveStream, System.Text.Encoding.UTF8);
+                ResHTML = readStream.ReadToEnd();
+                resp.Close();
+                readStream.Close();
+
+            }
+            catch (Exception exc)
+            { ResHTML = exc.Message; }
+
+            if (statusCodeResult == HttpStatusCode.OK)
+            {
+                var access = JsonConvert.DeserializeObject<Models.BodyResultToken200>(ResHTML).detail.access;
+                isGetToken = true;
+                return access;
+            }
+
+            return ResHTML;
+        }
+
+        public string ImageToBase64(Image image,
+            System.Drawing.Imaging.ImageFormat format)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                // Convert Image to byte[]
+                image.Save(ms, format);
+                byte[] imageBytes = ms.ToArray();
+
+                // Convert byte[] to Base64 String
+                string base64String = Convert.ToBase64String(imageBytes);
+                return base64String;
+            }
+        }
+
+        
+    }
+}
