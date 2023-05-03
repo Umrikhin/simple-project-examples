dc01dd9aa5b0f04c283b07b5b49d0f49acf1b724
Umrikhin <bono39@yandex.ru>
2023-05-03 13:19:10 +0300

    update model FileParcel in test

--- Parcels/TestParcels/Models/FileParcel.cs
+++ Parcels/TestParcels/Models/FileParcel.cs
@@ -10,8 +10,8 @@ namespace TestParcels.Models
     public class FileParcel
     {
         public Guid Id { get; set; }
-        public int IdUser { get; set; } = 1;
         public string CTERR { get; set; } = string.Empty;
+        public int IdUser { get; set; } = 1;
         public string StartFile { get; set; } = string.Empty;
         public DateTime DateStart { get; set; }
         public string RetFile { get; set; } = string.Empty;
